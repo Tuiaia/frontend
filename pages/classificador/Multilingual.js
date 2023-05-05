ce7f2746bfe26d32c749dac5b5c0b87b85e80811
LourdesOshiroIgarashi <lourdes.oshiro@ufms.br>
2023-05-05 06:12:41 -0400

    style(classificador): Edição do posicionamento e tamanho das imagens
    Edição do posicionamento e tamanho das imagens da seção multilingual no classificador

--- pages/classificador/Multilingual.js
+++ pages/classificador/Multilingual.js
@@ -12,18 +12,18 @@ const Multilingual = () => {
                 height: '850px',
             }}
         >
-            <div className="w-2/5 ml-[-12%]">
-                <Image src="arara-multilingual-classificador.svg" alt="Araras multilingual" width={850} height={600} />
+            <div className="w-3/5 ml-[-18%]">
+                <Image src="arara-multilingual-classificador.svg" alt="Araras multilingual" width={1000} height={600} />
             </div>
-            <div className="w-2/5 text-center">
-                <div className="text-secondary text-6xl font-bold mb-10">{MultilingualText.title}</div>
-                <div className="text-white text-3xl text-justify">
+            <div className="w-2/5 ml-[-15%]">
+                <div className="text-secondary text-6xl font-bold mb-10 text-center">{MultilingualText.title}</div>
+                <div className="text-white text-3xl">
                     {MultilingualText.paragraph_one}<br /><br />
                     {MultilingualText.paragraph_two}
                 </div>
             </div>
-            <div className="w-2/5 absolute right-[-5%]">
-                <Image src="/bandeiras-multilingual-classificador.svg" alt="Bandeiras multilingual" width={850} height={600} />
+            <div className="w-3/5 absolute right-[-18%]">
+                <Image src="/bandeiras-multilingual-classificador.svg" alt="Bandeiras multilingual" width={1000} height={600} />
             </div>
 
         </section>
