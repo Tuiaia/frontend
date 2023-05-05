bfebfbf764239b73ec97277791ce1372f3e203c3
LourdesOshiroIgarashi <lourdes.oshiro@ufms.br>
2023-05-05 14:57:40 -0400

    style(classificador): Redimensiona tamanho da seção de aplicação

--- pages/classificador/Aplicacao.js
+++ pages/classificador/Aplicacao.js
@@ -1,28 +1,33 @@
-import { AplicacaoText } from '@/data/text/classificador';
+import { MultilingualText } from '@/data/text/classificador';
+import Image from "next/image";
 
-const Aplicacao = () => {
+const Multilingual = () => {
     return (
         <section
-            className={'w-full bg-no-repeat bg-cover pb-700 flex'}
+            className="section-multilingual w-full flex items-center relative"
             style={{
-                backgroundImage: 'url("/aplicacao-classificador.svg")',
+                backgroundImage: 'url("/multilingual-classificador.svg")',
                 backgroundRepeat: 'no-repeat',
                 backgroundSize: 'cover',
-                height: '800px',
-                backgroundPosition: 'right center',
-            }}>
-            <div className={'flex justify-end items-center p-20'}>
-                <div className={'flex-1 max-w-5xl bg-p-bold h-full p-10 rounded-3xl pb-31'}>
-                    <div className={'text-secondary text-6xl font-bold mb-10'}>{AplicacaoText.title}</div>
-                    <div className={'text-white text-3xl text-justify'}>
-                        {AplicacaoText.paragraph_one}<br/><br/>
-                        {AplicacaoText.paragraph_two}<br/><br/>
-                        {AplicacaoText.paragraph_three}
-                    </div>
+                height: '850px',
+            }}
+        >
+            <div className="w-3/5 ml-[-18%]">
+                <Image src="arara-multilingual-classificador.svg" alt="Araras multilingual" width={1000} height={600} />
+            </div>
+            <div className="w-2/5 ml-[-15%]">
+                <div className="text-secondary text-6xl font-bold mb-10 text-center">{MultilingualText.title}</div>
+                <div className="text-white text-3xl">
+                    {MultilingualText.paragraph_one}<br /><br />
+                    {MultilingualText.paragraph_two}
                 </div>
             </div>
+            <div className="w-3/5 absolute right-[-18%]">
+                <Image src="/bandeiras-multilingual-classificador.svg" alt="Bandeiras multilingual" width={1000} height={600} />
+            </div>
+
         </section>
     )
 }
 
-export default Aplicacao
+export default Multilingual;
