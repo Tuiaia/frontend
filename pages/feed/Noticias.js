ba04e62ae605b7670bbbd5c4e7fbeff406d16c47
Hallyson Nobres <hallysonnobres@gmail.com>
2023-05-07 23:53:18 -0400

    feat(Feed): Finalizado cards de noticias com dados estáticos

--- pages/feed/Noticias.js
+++ pages/feed/Noticias.js
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import NoticiasCard from '@/pages/feed/NoticiasCard';
 
 const Noticias = () => {
     const date = new Date().toLocaleDateString('pt-BR');
@@ -9,32 +10,23 @@ const Noticias = () => {
             fonte: 'Infomoney',
             date: '20/04/2023',
             image: '/arara_vermelha_feed.svg',
-            link: 'https://www.infomoney.com.br/mercados/ibovespa-tem-leve-alta-com-exterior-positivo-e-antes-de-decisao-do-copom-veja-os-destaques/'
+            link: 'https://www.infomoney.com.br/mercados/ibovespa-tem-leve-alta-com-exterior-positivo-e-antes-de-decisao-do-copom-veja-os-destaques/',
         },
     ]
+    const testes = [0, 1, 2, 3, 4, 5]
 
     return (
         <section className={'w-full bg-primary flex flex-col items-center p-10 min-h-screen'}>
-            <div className={'flex items-center text-center p-20 w-full'}>
-                <span className={'text-white flex-1 text-6xl m-auto font-bold'}>{date}</span>
-                <Image src={'icone_filtro.svg'} alt={''} width={100} height={100}/>
+            <div className={'flex items-center text-center p-20 w-[63%]'}>
+                <span className={'text-white text-6xl m-auto font-bold'}>{`${date} - ${date}`}</span>
+                <Image className={'ml-16'} src={'icone_filtro.svg'} alt={''} width={50} height={50}/>
             </div>
-            <div className={'bg-white rounded-3xl p-4 py-10 flex gap-10'}>
-                <div className={'ml-6 w-full'}>
-                    <div className={'text-primary text-3xl font-bold mb-8'}>Fazenda lança pacote com 13 medidas para estimular crédito e destravar PPPs</div>
-                    <div className={'text-2xl'}>
-                        <span className={'text-secondary font-bold inline-block mr-4'}>InfoMoney</span>
-                        <span>(20/04/2023)</span>
-                        <Image className={'inline-block'} src={'share.svg'} alt={''} width={40} height={40} />
-                    </div>
-                </div>
-                <div>
+            {
+                testes? testes.map((noticia, index) => <NoticiasCard key={index} noticia={noticia} />) :
                     <div className={'flex justify-center items-center'}>
-                        <Image src={'positive_icon.svg'} alt={''} width={60} height={60} />
-                        <span>Positivo</span>
+                        <div className={'text-white text-6xl m-auto font-bold'}>Nenhuma notícia encontrada</div>
                     </div>
-                </div>
-            </div>
+            }
         </section>
     )
 }
