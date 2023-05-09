c8c935c199b08fef78aafb68c8f73d09ee91a797
Hallyson Nobres <hallysonnobres@gmail.com>
2023-05-09 00:04:51 -0400

    feat(Refinamento): Realizado refinamento da aplicação

--- pages/index.js
+++ pages/index.js
@@ -12,6 +12,9 @@ export default function Home() {
           <meta name="description" content="A melhor IA para classificar sentimento de notícias" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <link rel="icon" href="/favicon.ico" />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+          <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&display=swap" rel="stylesheet" />
         </Header>
         <main>
           <NossoTime />
