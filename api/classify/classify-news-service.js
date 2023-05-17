a3c6c8c8eb7cf9d7649d72ce5b91d9e7e4e62911
Hallyson Nobres <hallysonnobres@gmail.com>
2023-05-17 11:59:41 -0400

    feat(api) - adicionado timeout para rotas do sistema

--- api/classify/classify-news-service.js
+++ api/classify/classify-news-service.js
@@ -2,14 +2,21 @@ const url = 'http://35.199.123.76/'
 
 export default async function classifyNews(news) {
     try{
+        const timeout = 60000
+        const controller = new AbortController();
+        const idTimer = setTimeout(() => controller.abort(), timeout);
+        
         const response = await fetch(url, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify({news: news})
+            body: JSON.stringify({news: news}),
+            signal: controller.signal
         })
         
+        clearTimeout(idTimer);
+
         const json = await response.json()
         
         if (json.detail) {
